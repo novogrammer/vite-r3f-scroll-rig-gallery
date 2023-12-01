29310d97432a4530aab01289198cddd1d6a9189e
Yusuke Kawamoto <novogrammer@gmail.com>
2023-12-01 20:28:58 +0900

    DirectionalLightを調整

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,37 @@ import './App.css'
 import SectionAbout from './SectionAbout'
 import SectionGallery from './SectionGallery'
 import SectionHero from './SectionHero'
+import { useEffect } from 'react'
+import * as THREE from "three"
+import { useFrame } from '@react-three/fiber'
+
+function MyDirectionalLight(){
+  const directionalLight=new THREE.DirectionalLight();
+  directionalLight.intensity=1.0;
+  directionalLight.castShadow=true;
+
+  const directionalLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+  directionalLightShadowHelper.visible=false;
+  useFrame(()=>{
+    const width=window.innerWidth;
+    const height=window.innerHeight;
+    directionalLight.shadow.camera.near=0.1;
+    directionalLight.shadow.camera.far=1000;
+    directionalLight.position.set(0,100,500);
+    directionalLight.shadow.mapSize.set(1024,1024);
+    directionalLight.shadow.camera.left=width*-0.5;
+    directionalLight.shadow.camera.right=width*+0.5;
+    directionalLight.shadow.camera.top=height*+0.5 * 3;
+    directionalLight.shadow.camera.bottom=height*-0.5 * 3;
+    directionalLightShadowHelper.update();
+  })
+
+  return (<>
+    <primitive object={directionalLight} />
+    <primitive object={directionalLightShadowHelper}/>
+  </>);
+
+}
 
 function App() {
 
@@ -10,9 +41,7 @@ function App() {
     <>
       <GlobalCanvas shadows={true} camera={{fov:20}}>
         <ambientLight intensity={0.6} />
-        <directionalLight intensity={1.0} position={[0, 1, 5]} castShadow={true}>
-          <orthographicCamera attach="shadow-camera" near={1} far={10000} top={-1000} bottom={1000} left={-1000} right={1000}/>
-        </directionalLight>
+        <MyDirectionalLight/>
 
       </GlobalCanvas>
       <SmoothScrollbar config={{
