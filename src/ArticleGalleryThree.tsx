09c484c08482e27fd2faa7ea8568f7e35948c161
Yusuke Kawamoto <novogrammer@gmail.com>
2023-12-03 16:12:56 +0900

    くりぬいたような演出にする

--- src/ArticleGalleryThree.tsx
+++ src/ArticleGalleryThree.tsx
@@ -5,6 +5,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { Float } from "@react-three/drei";
 import { MutableRefObject, useRef } from "react";
 import { ScrollScene, UseCanvas } from "@14islands/r3f-scroll-rig";
+import { BufferGeometryUtils } from "three/examples/jsm/Addons.js";
 
 
 function Suzanne(props:Partial<PrimitiveProps>) {
@@ -26,7 +27,56 @@ function Suzanne(props:Partial<PrimitiveProps>) {
     <primitive {...merged}/>
   );
 }
+function MyBox(){
+  const w=6;
+  const h=4;
+  const d=6;
+  const boxGeometryInner=new THREE.BoxGeometry(w*0.999,h*0.999,d*0.999);
+  boxGeometryInner.applyMatrix4(new THREE.Matrix4().makeScale(-1, 1, 1));
+  const planeGeometryOuterLeft=new THREE.PlaneGeometry(d,h);
+  {
+    planeGeometryOuterLeft.applyMatrix4(new THREE.Matrix4().makeRotationY(90 * THREE.MathUtils.DEG2RAD));
+    planeGeometryOuterLeft.applyMatrix4(new THREE.Matrix4().makeTranslation(w*+0.5,0,0));
+  }
+  const planeGeometryOuterRight=new THREE.PlaneGeometry(d,h);
+  {
+    planeGeometryOuterRight.applyMatrix4(new THREE.Matrix4().makeRotationY(-90 * THREE.MathUtils.DEG2RAD));
+    planeGeometryOuterRight.applyMatrix4(new THREE.Matrix4().makeTranslation(w*-0.5,0,0));
+  }
+  const planeGeometryOuterTop=new THREE.PlaneGeometry(w,d);
+  {
+    planeGeometryOuterTop.applyMatrix4(new THREE.Matrix4().makeRotationX(-90 * THREE.MathUtils.DEG2RAD));
+    planeGeometryOuterTop.applyMatrix4(new THREE.Matrix4().makeTranslation(0,h*+0.5,0));
+  }
+  const planeGeometryOuterBottom=new THREE.PlaneGeometry(w,d);
+  {
+    planeGeometryOuterBottom.applyMatrix4(new THREE.Matrix4().makeRotationX(90 * THREE.MathUtils.DEG2RAD));
+    planeGeometryOuterBottom.applyMatrix4(new THREE.Matrix4().makeTranslation(0,h*-0.5,0));
+  }
 
+  const boxGeometryOuter=BufferGeometryUtils.mergeGeometries([
+    planeGeometryOuterLeft,
+    planeGeometryOuterRight,
+    planeGeometryOuterTop,
+    planeGeometryOuterBottom,
+  ],false);
+  boxGeometryInner.computeVertexNormals();
+  return (
+    <group position={[0,0,0 - 3]}>
+      <mesh receiveShadow={true}>
+        <bufferGeometry {...boxGeometryInner} />
+        <meshStandardMaterial roughness={0.2} metalness={0} />
+      </mesh>
+      <mesh receiveShadow={true} renderOrder={-1}>
+        <bufferGeometry {...boxGeometryOuter} />
+        <meshBasicMaterial colorWrite={false} />
+      </mesh>
+    </group>
+  )
+
+}
+
+// 3:2を前提にしている、高さ4m
 
 export default function ArticleGalleryThree() {
   const articleRef = useRef<HTMLElement|null>(null);
@@ -35,16 +85,13 @@ export default function ArticleGalleryThree() {
     <article ref={articleRef} className={styles["component"]}>
       <UseCanvas>
         <ScrollScene track={articleRef as MutableRefObject<HTMLElement>} >
-          {({...props})=>(<group scale={props.scale.xy.min() * 0.2}>
-              <mesh position={[0,0,0]} receiveShadow={true}>
-                <boxGeometry args={[4,3,6]}  />
-                <meshStandardMaterial roughness={0.2} metalness={0} side={THREE.BackSide} />
-              </mesh>
+          {({...props})=>(<group scale={props.scale.xy.min() * 0.25}>
+              <MyBox/>
               <Float>
-                <Suzanne position={[-1,0,-2]}/>
+                <Suzanne position={[-1,0.5,0 - 3]}/>
               </Float>
               <Float>
-                <Suzanne position={[1,0,0]}/>
+                <Suzanne position={[1,-0.5,2 - 3]}/>
               </Float>
 
             </group>
