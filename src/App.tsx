8a59f4d32528f80aab05b53a5fd49d5b42f0f5d9
Yusuke Kawamoto <novogrammer@gmail.com>
2023-12-07 18:11:42 +0900

    SoftShadows対応

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,28 @@ import './App.css'
 import SectionAbout from './SectionAbout'
 import SectionGallery from './SectionGallery'
 import SectionHero from './SectionHero'
-import * as THREE from "three"
-import { Canvas, useFrame } from '@react-three/fiber'
+// import * as THREE from "three"
+import { Canvas } from '@react-three/fiber'
 import { TunnelR3f } from './TunnelR3f'
-import { Preload } from '@react-three/drei'
-import { calcThreeWindowHeight } from './three_utils'
+import { Preload, SoftShadows } from '@react-three/drei'
 
 function MyDirectionalLight(){
-  const directionalLight=new THREE.DirectionalLight();
-  directionalLight.intensity=1.0;
-  directionalLight.castShadow=true;
 
-  const directionalLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
-  useFrame((state)=>{
-    const {camera}=state;
-    if(camera instanceof THREE.PerspectiveCamera){
-      const threeWindowHeight=calcThreeWindowHeight(camera);
-      const threeWindowWidth=threeWindowHeight*state.viewport.aspect;
-      directionalLight.shadow.camera.near=0.1;
-      directionalLight.shadow.camera.far=20;
-      directionalLight.position.set(0,1,5);
-      directionalLight.shadow.mapSize.set(1024,1024);
-      directionalLight.shadow.camera.left=threeWindowWidth*-0.5 * 2;
-      directionalLight.shadow.camera.right=threeWindowWidth*+0.5 * 2;
-      directionalLight.shadow.camera.top=threeWindowHeight*+0.5 * 3;
-      directionalLight.shadow.camera.bottom=threeWindowHeight*-0.5 * 3;
-      directionalLightShadowHelper.update();
-      }
-    directionalLightShadowHelper.visible=false;
-  })
+  return (<directionalLight position={[0,1,5]} intensity={1.0} castShadow shadow-mapSize={[1024*2,1024*2]}>
+    <orthographicCamera attach="shadow-camera" args={[-2,2,2,-2]}/>
+  </directionalLight>);
 
-  return (<>
-    <primitive object={directionalLight} />
-    <primitive object={directionalLightShadowHelper}/>
-  </>);
-
+}
+const Scene=()=>{
+  return (
+    <>
+      <SoftShadows />
+      <ambientLight intensity={0.6} />
+      <MyDirectionalLight/>
+      <TunnelR3f.Out/>
+      <Preload all/>
+    </>
+  );
 }
 
 function App() {
@@ -44,11 +31,8 @@ function App() {
   return (
     <>
       <div id="viewWrapper">
-        <Canvas shadows={true} camera={{fov:20}} style={{pointerEvents:"none"}}>
-          <ambientLight intensity={0.6} />
-          <MyDirectionalLight/>
-          <TunnelR3f.Out/>
-          <Preload all/>
+        <Canvas shadows camera={{fov:20}} style={{pointerEvents:"none"}}>
+          <Scene/>
         </Canvas>
       </div>
       <SectionHero/>
