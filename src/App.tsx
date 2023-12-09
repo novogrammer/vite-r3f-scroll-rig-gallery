1308666bf136f8414f7dac990c90b549e473f63f
Yusuke Kawamoto <novogrammer@gmail.com>
2023-12-09 13:49:43 +0900

    fovxからfovを計算し直す

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GlobalCanvas, SmoothScrollbar } from '@14islands/r3f-scroll-rig'
+import { GlobalCanvas, SmoothScrollbar,useCanvasStore } from '@14islands/r3f-scroll-rig'
 import './App.css'
 import SectionAbout from './SectionAbout'
 import SectionGallery from './SectionGallery'
@@ -6,6 +6,7 @@ import SectionHero from './SectionHero'
 import * as THREE from "three"
 import { useFrame } from '@react-three/fiber'
 import { SoftShadows } from '@react-three/drei'
+import { createContext, useContext,Dispatch,SetStateAction, useState } from 'react'
 
 function MyDirectionalLight(){
   const directionalLight=new THREE.DirectionalLight();
@@ -35,22 +36,58 @@ function MyDirectionalLight(){
 
 }
 
-function App() {
+type FovState = number;
+type SetStateActionFovState = Dispatch<SetStateAction<FovState>>;
 
+const FovContext=createContext<{
+  value:FovState,
+  setState:SetStateActionFovState
+}>({
+  value:50,
+  setState:()=>{},
+});
+
+function FovUpdateByFovx({fovx}:{fovx:number}){
+  const fovContext=useContext(FovContext);
+  const requestReflow=useCanvasStore((state)=>state.requestReflow);
+  
+  useFrame((state)=>{
+    
+    const fovy=Math.atan(Math.tan(fovx*THREE.MathUtils.DEG2RAD)/state.viewport.aspect)*THREE.MathUtils.RAD2DEG;
+    const epsilon=0.001;
+    if(epsilon<Math.abs(fovContext.value - fovy)){
+      fovContext.setState(()=>fovy);
+      console.log("update");
+      requestReflow();
+    }
+
+  })
+  return null;
+}
+
+
+function App() {
+  const [fov,setFov]=useState<FovState>(20);
+  useCanvasStore
+  
   return (
     <>
-      <GlobalCanvas shadows={true} camera={{fov:20}} scaleMultiplier={0.01}>
-        <ambientLight intensity={0.6} />
-        <MyDirectionalLight/>
-        <SoftShadows focus={0.25} />
+      <FovContext.Provider value={{value:fov,setState:setFov}}>
+        <GlobalCanvas shadows={true} camera={{fov:fov}} scaleMultiplier={0.01}>
+          <FovUpdateByFovx fovx={20}/>
 
-      </GlobalCanvas>
-      <SmoothScrollbar config={{
-          duration:0.25,
-      }} />
-      <SectionHero/>
-      <SectionGallery/>
-      <SectionAbout/>
+          <ambientLight intensity={0.6} />
+          <MyDirectionalLight/>
+          <SoftShadows focus={0.25} />
+
+        </GlobalCanvas>
+        <SmoothScrollbar config={{
+            duration:0.25,
+        }} />
+        <SectionHero/>
+        <SectionGallery/>
+        <SectionAbout/>
+      </FovContext.Provider>
     </>
   )
 }
